c13a72d566a3cba308f6f421db71aab14fb307a0
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-26 13:35:13 -0300

    feat: :sparkles: Create Invert String function

--- index.js
+++ index.js
@@ -11,5 +11,17 @@ function checkFibonacci(n) {
       return `O número ${n} não pertence à sequência de Fibonacci.`;
   }
 }
+
 console.log("\n Verificar se um número pertence à sequência de Fibonacci: \n");
 console.log(checkFibonacci(9));
+
+function invertString(str) {
+  let InvertedString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+      InvertedString += str[i];
+  }
+  return InvertedString;
+}
+
+console.log("\n Inverter uma string: \n");
+console.log(invertString("Hello World!"));
